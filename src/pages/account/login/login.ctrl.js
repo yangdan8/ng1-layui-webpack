710fe8d33813ade8ea007435bcce645e4c1d7e05
yd <yd@yd.com>
2017-10-24 11:30:52 +0800

    模拟测试调试功能debugger;

--- src/pages/account/login/login.ctrl.js
+++ src/pages/account/login/login.ctrl.js
@@ -7,6 +7,7 @@ class AccountLoginController {
         this.$scope = $scope;
         this.title = '这里是登录页';
         $.ajax('/api/5aV1bjqh_Q23odCf/static/superman/css/mt_min_d0e7c6d2.css').done((data) => {
+            debugger;
             console.log('来自百度的数据', data);
         });
     }
